8c3132e5c65055a50026f6b853c2ab0fa370101e
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2021-03-02 10:26:38 +0100

    Initial commit

---
+++ src/test/java/se/lexicon/g34/bl/jpa_workshop/JpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g34.bl.jpa_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
